bcce838b6d26a71e19306e89e1f9b75b9aeeba07
helouazizi <ouazizi2code@gamil.com>
2025-01-21 21:05:45 +0100

    add create post handler

--- cmd/main.go
+++ cmd/main.go
@@ -21,6 +21,7 @@ func main() {
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/create_account", auth.Register)
 	http.HandleFunc("/log_in", auth.Log_in)
+	http.HandleFunc("/create_Post", handlers.Craete_post)
 	http.HandleFunc("/static/", handlers.Serve_Static)
 	fmt.Println("server is running on port 8080 ... http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
