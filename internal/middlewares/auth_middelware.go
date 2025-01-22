2b773f9d7255f10811bbe2e00b9d6a4c006cd7e9
helouazizi <ouazizi2code@gamil.com>
2025-01-22 16:37:28 +0100

    add middelwares pkg for authentification

---
+++ internal/middlewares/auth_middelware.go
@@ -0,0 +1,34 @@
+// internal/middleware/middleware.go
+package middlewares
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func Test() {
+	fmt.Println("test")
+}
+
+func LoginMidlleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// lets log the request
+		log.Printf("%v recieved a request >>   Method: %s, URL: %s, Client IP: %s", r.Header.Get("Date"), r.Method, r.URL.Path, r.RemoteAddr)
+		log.Print("------------------------")
+		// now lets check for authorisation
+		authorisationHrader := r.Header.Get("Authorization")
+		if authorisationHrader == "" || !strings.HasPrefix(authorisationHrader, "aghyor ") || !validToken(authorisationHrader[7:]) {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+		// now call the next hamdel in the chain
+		next.ServeHTTP(w, r)
+	})
+
+}
+
+func validToken(token string) bool {
+	return token == "boda"
+}
