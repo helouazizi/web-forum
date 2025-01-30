990a3dd636955fdf5b26aba765a8943c19df02c0
helouazizi <ouazizi2code@gmail.com>
2025-01-30 14:59:09 +0100

    update the logout logic

--- internal/auth/log_out.go
+++ internal/auth/log_out.go
@@ -1,11 +1,8 @@
 package auth
 
 import (
-	"fmt"
-	"forum/internal/database"
 	"forum/internal/handlers"
 	"net/http"
-	"time"
 )
 
 func Log_out(w http.ResponseWriter, r *http.Request) {
@@ -18,40 +15,16 @@ func Log_out(w http.ResponseWriter, r *http.Request) {
 	// lets check in first that is already have a session
 	if !IsCookieSet(r, "token") {
 		w.WriteHeader(http.StatusNotFound)
-		pages.All_Templates.ExecuteTemplate(w, "error.html", "page not founttt")
+		pages.All_Templates.ExecuteTemplate(w, "error.html", "page not found")
 		return
 	}
 	http.SetCookie(w, &http.Cookie{
-		Name:     "token",         // name of the cookie
-		Value:    "",              // clear the cookie value
-		Expires:  time.Unix(0, 0), // set expiration time to a time in the past
-		Path:     "/",             // scope of the cookie
-		HttpOnly: true,            // prevent JavaScript access
-		Secure:   true,            // ensure cookie is only sent over HTTPS
+		Name:   "token", // name of the cookie
+		Value:  "",      // clear the cookie value
+		MaxAge: -1,      // set expiration time to a time in the past
+		Path:   "/",     // scope of the cookie
+		//HttpOnly: true,    // prevent JavaScript access
+		//Secure:   true,    // ensure cookie is only sent over HTTPS
 	})
 	http.Redirect(w, r, "/", http.StatusFound)
-}
-
-func Update_Token(w http.ResponseWriter, r *http.Request, tokenName string, token string) {
-	pages := handlers.Pagess
-	cokie, err := r.Cookie(tokenName)
-	if err != nil {
-		// If no cookie exists, handle appropriately
-		w.WriteHeader(http.StatusUnauthorized)
-		pages.All_Templates.ExecuteTemplate(w, "error.html", "Unauthorized: No token provided")
-		return
-	}
-
-	fmt.Println("Token from cookie:", cokie.Value)
-
-	// Update the token to NULL or an empty string in the database
-	query := "UPDATE users SET token = $1 WHERE token = $2"
-	_, err = database.Database.Exec(query, token, cokie.Value)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		pages.All_Templates.ExecuteTemplate(w, "error.html", "Internal server error")
-		return
-	}
-
-	fmt.Println("Token removed from database")
 }
