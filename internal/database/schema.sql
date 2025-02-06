5c771fb3c5148216b7150516ca3d564a81a5f731
hassan el ouazizi <ouazizi2code@gmail.com>
2025-02-06 16:19:36 +0100

    update database

--- internal/database/schema.sql
+++ internal/database/schema.sql
@@ -1,4 +1,5 @@
 /* create a table called "users" */
+/* Expiration date to be added and also */
 CREATE TABLE
     IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -6,7 +7,7 @@ CREATE TABLE
         userEmail VARCHAR(100) NOT NULL,
         userPassword VARCHAR(255) NOT NULL,
         token VARCHAR(255) NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP /*for the user*/
     );
 
 -- table of session ??
@@ -19,6 +20,7 @@ CREATE TABLE IF NOT EXISTS posts (
     content TEXT NOT NULL,
     total_likes INT DEFAULT 0 CHECK (total_likes >= 0),
     total_dislikes INT DEFAULT 0 CHECK (total_dislikes >= 0),
+    total_comments INT DEFAULT 0 CHECK (total_comments >= 0),
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
@@ -81,7 +83,7 @@ CREATE TABLE IF NOT EXISTS post_reaction (
 --CREATE INDEX IF NOT EXISTS idx_user_post ON likes (user_id, post_id);
 
 CREATE TABLE
-    IF NOT EXISTS dislikes (
+    IF NOT EXISTS comment_reaction (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         user_id INTEGER NOT NULL,
         post_id INTEGER NOT NULL,
