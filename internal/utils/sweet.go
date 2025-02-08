1708301bc82b1d76f736ebc5556af5628ecf04bb
hassan el ouazizi <ouazizi2code@gmail.com>
2025-02-08 10:30:21 +0100

    add cookie function

--- internal/utils/sweet.go
+++ internal/utils/sweet.go
@@ -3,6 +3,7 @@ package utils
 
 import (
 	"database/sql"
+	"net/http"
 	"net/mail"
 	"regexp"
 	"strings"
@@ -68,4 +69,25 @@ func IsExist(collumn0, collumn1, value string) (string, bool) {
 	}
 
 	return pass, true
+}
+
+func IsCookieSet(r *http.Request, cookieName string) bool {
+	cookie, err := r.Cookie(cookieName)
+	if err != nil {
+		return false
+	}
+	if cookie.Value == "" {
+		return false
+	}
+	// lets extract the token value from the cookie and compare it with the one we have in databse
+
+	var tokenExist bool
+	// lets extract the token from users table
+	// be care full with  no token
+	tokenErr := database.Database.QueryRow("SELECT EXISTS (SELECT 1 FROM users WHERE token = $1)", cookie.Value).Scan(&tokenExist)
+	if tokenErr != nil || !tokenExist {
+		return false
+	}
+
+	return true
 }
