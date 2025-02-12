e69ffa2fa5b9fcb64701c1ab9c7f5591e0fa2a12
helouazizi <ouazizi2code@gmail.com>
2025-02-12 19:55:43 +0100

    add experation date into loging

--- internal/auth/login.go
+++ internal/auth/login.go
@@ -1,82 +1,74 @@
 package auth
 
 import (
-	"database/sql"
+	"log"
 	"net/http"
+	"reflect"
+	"time"
 
 	"forum/internal/database"
-	"forum/internal/handlers"
+	"forum/internal/models"
+	"forum/internal/utils"
+	"forum/pkg/logger"
 
 	"github.com/google/uuid"
-	"golang.org/x/crypto/bcrypt"
 )
 
-func Log_in(w http.ResponseWriter, r *http.Request) {
-	pages := handlers.Pagess
+var (
+	FormErrors = models.FormErrors{}
+	FormsData  = models.FormsData{}
+)
+
+func LogIn(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		pages.All_Templates.ExecuteTemplate(w, "error.html", "Method Not Allowed")
+		utils.RenderTemplate(w, "error.html", models.MethodNotAllowed, http.StatusMethodNotAllowed)
 		return
 	}
-	if IsCookieSet(r, "token") {
-		w.WriteHeader(http.StatusNotFound)
-		pages.All_Templates.ExecuteTemplate(w, "error.html", "Page Not Found")
+	db, err := database.NewDatabase()
+	if err != nil {
+		logger.LogWithDetails(err)
+		utils.RenderTemplate(w, "error.html", models.InternalServerError, http.StatusInternalServerError)
 		return
 	}
+	defer db.Close()
 	UserName := r.FormValue("userName")
-	Password := r.FormValue("userPassword")
-
-	if UserName == "" || Password == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		pages.All_Templates.ExecuteTemplate(w, "error.html", "Bad Request")
-		return
-	}
-
-	// lets check the valid data from user
-	var pasword string
-	var username string
-	err := database.Database.QueryRow("SELECT userName , userPassword  FROM users WHERE  username= $1 ", UserName).Scan(&username, &pasword)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			w.WriteHeader(http.StatusUnauthorized)
-			pages.All_Templates.ExecuteTemplate(w, "sign_in.html", "Invalid Password or UserName") // should execute login page here for no rows err
-			return
-		}
-		w.WriteHeader(http.StatusInternalServerError)
-		pages.All_Templates.ExecuteTemplate(w, "error.html", "internal server error")
-		return
-
-	}
-	if err := bcrypt.CompareHashAndPassword([]byte(pasword), []byte(Password)); err != nil {
-		w.WriteHeader(http.StatusUnauthorized)
-		pages.All_Templates.ExecuteTemplate(w, "sign_in.html", "Invalid Password or UserName")
-		return
-	}
-	// lets update the soken
 	Token := uuid.New().String()
-	query := "UPDATE users SET token = ? WHERE userName = ?"
-	res, err := database.Database.Exec(query, Token, username)
+	stm, err := db.Prepare("UPDATE users SET token = ? , token_created_at = ? , expiration_date = ? where userName = ?")
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		pages.All_Templates.ExecuteTemplate(w, "error.html", "internal server error")
+		logger.LogWithDetails(err)
+		utils.RenderTemplate(w, "error.html", models.InternalServerError, http.StatusInternalServerError)
 		return
 	}
-	rowsAffected, _ := res.RowsAffected()
-	// it this statement the user not exist
-	if rowsAffected == 0 {
-		w.WriteHeader(http.StatusUnauthorized)
-		pages.All_Templates.ExecuteTemplate(w, "sign_in.html", "Invalid Password or UserName")
+	_, err = stm.Exec(Token, time.Now(), time.Now().Add(60*time.Minute), UserName)
+	if err != nil {
+		logger.LogWithDetails(err)
+		utils.RenderTemplate(w, "error.html", models.InternalServerError, http.StatusInternalServerError)
 		return
 	}
-	// after all that set the token
 	cookie := &http.Cookie{
 		Name:   "token",
 		Value:  Token,
 		MaxAge: 3600,
 		Path:   "/",
 	}
+
 	http.SetCookie(w, cookie)
-	//r.AddCookie(cookie)
-	//log.Println(UserName, "logged in")
-	http.Redirect(w, r, "/", http.StatusSeeOther)
+
+	log.Println(UserName, "logged in")
+
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+func IsValidFormValues(FormErrors models.FormErrors) (*models.Data, bool) {
+	values := reflect.ValueOf(FormErrors)
+	for i := 0; i < values.NumField(); i++ {
+		if values.Field(i).String() != "" {
+			data := &models.Data{
+				FormsData: FormsData,
+			}
+			data.FormsData.FormErrors = FormErrors
+			return data, false
+		}
+	}
+	return &models.Data{}, true
 }
