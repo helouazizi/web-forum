5031742205735bd0da1776104a08ae51b1ee325c
helouazizi <ouazizi2code@gmail.com>
2025-02-12 19:59:58 +0100

    update auth logic

--- internal/middlewares/auth_middelware.go
+++ internal/middlewares/auth_middelware.go
@@ -2,27 +2,50 @@
 package middlewares
 
 import (
-	"log"
 	"net/http"
-	"strings"
+	"time"
+
+	"forum/internal"
+	"forum/internal/auth"
+	"forum/internal/models"
+	"forum/internal/utils"
+	"forum/pkg/logger"
 )
 
-func Auth_Midlleware(next http.Handler) http.Handler {
+var auth_rateLimiter = auth.NewRateLimiter(30, time.Second) // 30 requests per second limit
+func Auth_Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// lets log the request
-		log.Printf("%v recieved a request >>   Method: %s, URL: %s, Client IP: %s", r.Header.Get("Date"), r.Method, r.URL.Path, r.RemoteAddr)
-		log.Print("------------------------")
-		// now lets check for authorisation
-		authorisationHrader := r.Header.Get("Authorization")
-		if authorisationHrader == "" || !strings.HasPrefix(authorisationHrader, "aghyor ") || !validToken(authorisationHrader[7:]) {
-			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		// Check if the request contains a token
+		ip := r.RemoteAddr
+		pages := internal.Templates
+		// Check rate limit (applies to both login and registration)
+		if auth_rateLimiter.CheckRateLimit(ip) {
+			w.WriteHeader(http.StatusTooManyRequests)
+			pages.ExecuteTemplate(w, "error.html", "Too many requests. Please try again later.")
 			return
 		}
-		// now call the next hamdel in the chain
+
+		if !utils.IsCookieSet(r, "token") {
+			http.Redirect(w, r, "/login", http.StatusFound) // Use StatusFound (302)
+			return
+		}
+		user, err := utils.UserData(r, "token", "")
+		if err != nil {
+			logger.LogWithDetails(err)
+			utils.RenderTemplate(w, "error.html", models.InternalServerError, http.StatusInternalServerError)
+			return
+		}
+		ok, err := utils.CheckTokenExpired(user)
+		if err != nil {
+			logger.LogWithDetails(err)
+			utils.RenderTemplate(w, "error.html", models.InternalServerError, http.StatusInternalServerError)
+			return
+		}
+		if ok {
+			http.Redirect(w, r, "/login", http.StatusSeeOther)
+			return
+		}
+
 		next.ServeHTTP(w, r)
 	})
-}
-
-func validToken(token string) bool {
-	return token == "boda"
 }
